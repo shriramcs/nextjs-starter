e71f9fcea02081707490c4fe9bc257f91b912ecc
shriramcs <shriramcs@gmail.com>
2024-12-30 13:21:47 +0100

    add home link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
-  return <div>About</div>;
+  return <div>
+    <Link href="/">
+        Home
+      </Link>
+    About
+  </div>;
 }
